79c05b8f492b4a7ce9d9b1e616f60e9ce0eadfc1
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-02-09 13:15:17 +0700

    Add popover to mark word as correct

--- src/components/steps/Step3.js
+++ src/components/steps/Step3.js
@@ -3,8 +3,17 @@ import {
   Row,
   Col,
   Progress,
-  Button
+  Button,
+  Popover
 } from 'antd'
+
+const markWordAsCorrect = (
+  <div>
+    Expected: <b>dummy</b><br />
+    You said: <b>yabba dabba doo</b><br /><br />
+    <Button type='default' size='default'>Mark as correct</Button>
+  </div>
+)
 
 const Step3 = (props) => {
   return (
@@ -12,7 +21,7 @@ const Step3 = (props) => {
       <Row gutter={30}>
         <Col span={12}>
           <h2>Original</h2>
-          <p>
+          <p className='diff-content'>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
             took a galley of type and scrambled it to make a type specimen book. It has survived not only five
@@ -23,8 +32,17 @@ const Step3 = (props) => {
         </Col>
         <Col span={12}>
           <h2>Recorded</h2>
-          <p>
-            Lorem Ipsum is simply <span className='text-error'>yabba dabba</span> text of the printing and typesetting industry.
+          <p className='diff-content'>
+            Lorem Ipsum is simply
+            <Popover placement='top' title='Incorrect word' content={markWordAsCorrect} trigger='click'>
+              &nbsp;
+              <span className='diff-incorrect'>
+                <span className='text-missing'>dummy</span>&nbsp;
+                <span className='text-error'>yabba dabba doo</span>
+              </span>
+              &nbsp;
+            </Popover>
+            text of the printing and typesetting industry.
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
             took a galley of type and scrambled it to make a type specimen book. It has survived not only five
             centuries, but also the leap into electronic typesetting, remaining
@@ -38,7 +56,7 @@ const Step3 = (props) => {
         <h3>
           Memorization level:
         </h3>
-        <Col span={24} xs={{ span: 24 }} lg={{ offset: 6, span: 12}}>
+        <Col span={24} xs={{ span: 24 }} lg={{ offset: 6, span: 12 }}>
           <Progress percent={90} />
         </Col>
       </Row>
