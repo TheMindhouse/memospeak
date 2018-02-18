4e50389cdfefc65a75a6b55985a9acd56ac10724
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-02-18 17:50:25 +0700

    Add new logo and header style

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import elephant from '../elephant.svg'
-import { Row, Layout } from 'antd'
-
-const { Header } = Layout
+import logo from '../assets/images/memospeak-logo.png'
+import { Row } from 'antd'
 
 const AppHeader = () => {
   return (
-    <Header className='header'>
-      <Row type='flex' align='middle'>
-        <img src={elephant} alt='Memospeak' className='header-logo' />
+    <Row type='flex' align='middle' justify='center' className="header">
+      <img src={logo} alt='Memospeak' className='header-logo' />
+      <div>
         <span className='header-name'>Memospeak</span>
-      </Row>
-    </Header>
+        <span className='header-subtitle'>Train your memory by memorizing texts</span>
+      </div>
+    </Row>
   )
 }
 
