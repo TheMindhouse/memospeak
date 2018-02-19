6a181f8b467d40d8d683ac21a015279bee2d8bc4
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-02-20 00:29:57 +0700

    More example texts

--- src/helpers/exampleTexts.js
+++ src/helpers/exampleTexts.js
@@ -3,29 +3,77 @@ export const exampleTexts = [
     language: 'English',
     languageCode: 'en-US',
     texts: [
-      ['Lose Yourself - Eminem',
+      ['Never Gonna Give You Up',
+        `Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you`],
+        ['Lose Yourself',
         `You better lose yourself in the music, the moment 
 You own it, you better never let it go 
 You only get one shot, do not miss your chance to blow 
-This opportunity comes once in a lifetime`]
+This opportunity comes once in a lifetime, yo`]
     ]
   },
   {
     language: 'Spanish',
     languageCode: 'es-ES',
     texts: [
-      ['Duele El Corazon - Enrique Iglesias',
-        `Si te vas yo también me voy 
+      ['Duele El Corazon',
+        `Solo en tu boca 
+Yo quiero acabar 
+Todos esos besos 
+Que te quiero dar 
+A mí no me importa 
+Que duermas con él 
+Porque sé que sueñas 
+Con poderme ver 
+Mujer qué vas a hacer 
+Decídete pa' ver 
+Si te quedas o te vas 
+Sino no me busques más
+
+Si te vas yo también me voy 
 Si me das yo también te doy 
 Mi amor 
 Bailamos hasta las diez 
 Hasta que duelan los pies`],
-      ['Despacito - Luis Fonsi',
+      ['Despacito',
         `Despacito 
 Quiero respirar tu cuello despacito 
 Deja que te diga cosas al oído 
 Para que te acuerdes si no estás conmigo
-Despacito`]
+
+Despacito 
+Quiero desnudarte a besos despacito 
+Firmo en las paredes de tu laberinto 
+Y hacer de tu cuerpo todo un manuscrito`],
+      ['Cancion del Mariachi',
+        `Soy un hombre muy honrado
+Que me gusta lo mejor
+Las mujeres no me faltan, ni el dinero, ni el amor
+
+Jineteando en mi caballo
+Por la sierra yo me voy
+Las estrellas y la luna, ellas me dicen dónde voy
+
+Ay, ay, ay, ay
+Ay, ay mi amor
+Ay mi morena de mi corazón
+
+Me gusta tocar la guitarra
+Me gusta cantar el sol
+Mariachi me acompaña cuando canto mi canción
+
+Me gusta tomar mis copas
+Aguardiente es lo mejor
+También el tequila blanco con su sal le da sabor
+
+Ay, ay, ay, ay
+Ay, ay mi amor
+Ay mi morena de mi corazón`]
     ]
   }
 ]
