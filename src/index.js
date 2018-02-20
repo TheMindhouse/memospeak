ba62a43b2088c6adb99d5607ba52d30b555c9915
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-02-20 14:48:09 +0700

    Require https

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css'
 import AppWrapper from './AppWrapper'
 import registerServiceWorker from './registerServiceWorker'
 
+// Require HTTPS
+if (window.location.protocol !== 'https:') {
+  window.location = 'https://' + window.location.host
+}
+
 ReactDOM.render(<AppWrapper />, document.getElementById('root'))
 registerServiceWorker()
