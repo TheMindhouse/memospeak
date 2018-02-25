111e9f48183ef3ea1526d211661b7d6231b8c67c
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-02-25 22:38:59 +0700

    Update header and add description

--- src/components/AppHeaderName.js
+++ src/components/AppHeaderName.js
@@ -4,7 +4,7 @@ const AppHeaderName = () => {
   return (
     <div>
       <span className='header-name'>Memospeak</span>
-      <span className='header-subtitle'>Train your memory by memorizing texts</span>
+      <span className='header-subtitle'>Memorize texts with Speech Recognition</span>
     </div>
   )
 }
